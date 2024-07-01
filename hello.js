9aca24c29d1a2cb9d0b572a64b776276854b65f0
Jhenaidha Sadar her power project web Section <mustakali444888@gmail.com>
2024-07-01 17:17:47 +0600

    some update

---
+++ hello.js
@@ -0,0 +1,17 @@
+
+
+var myvillage= 'Bahadurpur'
+console.log(typeof myvillage)
+
+var myfather = 'Hossain'
+console.log(myfather);
+
+var mymother ="mst"
+console.log(typeof mymother);
+
+var myvillage ='98765434'
+console.log(typeof myvillage);
+
+var mobnum=014000;
+console.log(typeof mobnum)
+
